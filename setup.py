e70e1004dae65bc1269e4574c308ed83e857706c
Phil Turner <philip.turner9@nhs.net>
2019-04-16 14:57:43 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name="ui_automation_core",
+   version="0.1",
+   description="Module providing common functionality for UI automation test packs",
+   author="Phil Turner",
+   author_email="philip.turner9@nhs.net",
+   packages=["ui_automation_core"],
+   install_requires=["selenium"]
+)
