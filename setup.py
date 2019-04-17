d16913c61e2f63484bddafc42b0efabef3779bb9
Phil Turner <philip.turner9@nhs.net>
2019-04-17 10:04:07 +0100

    Fixed folder structure

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
    description="Module providing common functionality for UI automation test packs",
    author="Phil Turner",
    author_email="philip.turner9@nhs.net",
-   packages=["ui_automation_core"],
+   packages=["ui_automation_core", "ui_automation_core.utilities"],
    install_requires=["selenium==3.11.0"]
 )
