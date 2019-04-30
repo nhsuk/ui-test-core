937141125966e76f699e61140feef905438fb7e2
Phil Turner <philip.turner9@nhs.net>
2019-04-30 11:44:48 +0000

    Merged PR 589: CITR-285
    Fix version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name="ui_automation_core",
-   version="0.2.5",
+   version="0.2.0",
    description="Module providing common functionality for automation test packs",
    packages=["ui_automation_core", "ui_automation_core.utilities"],
    install_requires=["selenium==3.11.0", "PyHamcrest==1.9.0", "requests==2.21.0", "behave==1.2.6"]
