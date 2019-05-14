f49fb52ff509b57f98629230613da333315f65fc
Phil Turner <philip.turner9@nhs.net>
2019-05-14 10:22:57 +0000

    Merged PR 681: CIRT-268b
    Move requirement for urllib3 into automation core - the specific version is required for requests.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
    name="ui_automation_core",
-   version="0.3.0",
+   version="0.3.1",
    description="Module providing common functionality for automation test packs",
    packages=["ui_automation_core", "ui_automation_core.utilities"],
-   install_requires=["selenium==3.141.0", "PyHamcrest==1.9.0", "requests==2.21.0", "behave==1.2.6"]
+   install_requires=["selenium==3.141.0", "PyHamcrest==1.9.0", "urllib3==1.24.3", "requests==2.21.0", "behave==1.2.6"]
 )
