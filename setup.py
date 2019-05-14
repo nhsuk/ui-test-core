2a43055e6a2769ff9f5a2fd24467948281702e8e
Phil Turner <philip.turner9@nhs.net>
2019-05-14 09:47:36 +0000

    Merged PR 679: Update Selenium
    Update the required Selenium package to the latest version

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
    name="ui_automation_core",
-   version="0.2.0",
+   version="0.3.0",
    description="Module providing common functionality for automation test packs",
    packages=["ui_automation_core", "ui_automation_core.utilities"],
-   install_requires=["selenium==3.11.0", "PyHamcrest==1.9.0", "requests==2.21.0", "behave==1.2.6"]
+   install_requires=["selenium==3.141.0", "PyHamcrest==1.9.0", "requests==2.21.0", "behave==1.2.6"]
 )
