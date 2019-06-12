831f2b45a564d65139740b5810189d78d257a586
Girirajaiah <v.girirajaiah@accenture.com>
2019-06-12 10:33:38 +0100

    Updated Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name="ui_automation_core",
-   version="1.0.1",
+   version="1.1.0",
    description="Module providing common functionality for automation test packs",
    packages=["ui_automation_core", "ui_automation_core.utilities"],
    install_requires=["selenium==3.141.0",
