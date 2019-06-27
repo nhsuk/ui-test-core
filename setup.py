98aa1e17c058db089a7ae3d675a2a6dfa8ae4d6e
Tony Lawson <tony.lawson3@nhs.net>
2019-06-27 11:19:19 +0000

    Merged PR 1087: added datetime_handler utility
    added datetime_handler utility

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
    name="ui_automation_core",
-   version="1.1.5",
+   version="1.2.0",
    description="Module providing common functionality for automation test packs",
    packages=["ui_automation_core", "ui_automation_core.utilities"],
    install_requires=["selenium==3.141.0",
                      "PyHamcrest==1.9.0",
                      "urllib3==1.24.3",
                      "requests==2.21.0",
-                     "behave==1.2.6"
+                     "behave==1.2.6",
+                     "python-dateutil==2.8.0"
                      ]
 )
---
+++ ui_automation_core/utilities/datetime_handler.py
@@ -0,0 +1,35 @@
+"""
+A useful collection of date and time utility methods
+"""
+import datetime
+from dateutil.relativedelta import relativedelta
+
+
+def get_current_date():
+    """
+    Uses the standard datetime lib to return date.today()
+    :return: Date in YYY-MM-DD format
+    """
+    return datetime.date.today()
+
+
+def get_date_altered_by_days(day_difference, current_date=datetime.date.today()):
+    """
+    Generates a date offset by a number of days
+    Can be a date in the future or in past, by passing in a positive or negative day_difference
+    :param day_difference: int number of days to offset from current date
+    :param current_date: optional. can be used to set a different starting date to offset from. Used for unit tests
+    :return:
+    """
+    return current_date + relativedelta(days=day_difference)
+
+
+def get_date_altered_by_months(month_difference, current_date=datetime.date.today()):
+    """
+    Generates a date offset by a number of days
+    Can be a date in the future or in past, by passing in a positive or negative month_difference
+    :param month_difference: int number of months to offset from current date
+    :param current_date: optional. can be used to set a different starting date to offset from. Used for unit tests
+    :return:
+    """
+    return current_date + relativedelta(months=month_difference)
