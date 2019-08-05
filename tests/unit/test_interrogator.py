58397443593992dcf2b9a6e4ec365217d37d7ced
Giuseppe Torchia <giuseppe.torchia1@nhs.net>
2019-08-05 11:47:44 +0000

    Merged PR 1381: TM-571
    Added a new method to delete a single cookie and refresh the related page

--- tests/unit/test_interrogator.py
+++ tests/unit/test_interrogator.py
@@ -1,4 +1,4 @@
-from hamcrest import assert_that, equal_to
+from hamcrest import assert_that, equal_to, contains
 
 from ui_automation_core.finder import Finder
 from ui_automation_core.interrogator import Interrogator
@@ -6,10 +6,20 @@ from ui_automation_core.interrogator import Interrogator
 
 class MockDriver(object):
 
+    def __init__(self, deleted_cookies=[]):
+        self.deleted_cookies = deleted_cookies
+        self.refresh_count = 0
+
     def get_cookies(self):
         mock_cookies = [{"name": "nhsuk-cookie-consent", "value": "%7B%22preferences%22%3Atrue%7D"},
                         {"name": "s_getNewRepeat", "value": "1564127000350-Repeat"}]
         return mock_cookies
+
+    def delete_cookie(self, cookie_name):
+        self.deleted_cookies.append(cookie_name)
+
+    def refresh(self):
+        self.refresh_count += 1
 
 
 def test_get_value_from_cookie_with_empty_string():
@@ -44,7 +54,19 @@ def test_get_value_from_cookie_with_wrong_key_value():
     assert_that(result, equal_to(""), f"Incorrect cookie value when searching for name: '{name}'")
 
 
+def test_clear_cookie_and_refresh_page_deletes_the_cookie():
+    finder = Finder("driver", "logger")
+    driver = MockDriver()
+    test_interrogator = Interrogator(driver, "logger", finder)
+    name = "Banner717"
+    test_interrogator.clear_cookie_and_refresh_page(name)
+    assert_that(driver.deleted_cookies, contains("Banner717"), "The cookie should be deleted")
 
 
-
-
+def test_clear_cookie_and_refresh_page_performs_a_refresh():
+    finder = Finder("driver", "logger")
+    driver = MockDriver()
+    test_interrogator = Interrogator(driver, "logger", finder)
+    name = "Banner717"
+    test_interrogator.clear_cookie_and_refresh_page(name)
+    assert_that(driver.refresh_count, equal_to(1), "The cookie  should be refreshed once")
