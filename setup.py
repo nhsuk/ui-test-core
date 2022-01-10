089a93a3f375993812c3c5946f18985f99b42a6f
ghufran <29932028+ghufrankhan@users.noreply.github.com>
2022-01-10 11:33:24 +0000

    updated packages (#31)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="uitestcore",
-    version="7.0.1",
+    version="7.0.2",
     description="Package providing common functionality for UI automation test packs",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -13,15 +13,15 @@ setup(
     homepage="https://github.com/nhsuk/ui-test-core/",
     packages=["uitestcore", "uitestcore.utilities"],
     install_requires=[
-        "certifi==2021.5.30",
+        "certifi==2021.10.8",
         "chardet==4.0.0",
-        "idna==2.10",
+        "idna==3.3",
         "pyhamcrest==2.0.2",
-        "python-dateutil==2.8.1",
-        "requests==2.25.1",
+        "python-dateutil==2.8.2",
+        "requests==2.26.0",
         "selenium==3.141.0",
         "six==1.16.0",
-        "urllib3==1.26.5"
+        "urllib3==1.26.7"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
