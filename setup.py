3047b3832daabd18964c26aed59358430d43629d
Ghufran Khan <guffyk@googlemail.com>
2023-06-08 13:01:48 +0100

    Updated Selenium to 4.10.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="uitestcore",
-    version="10.3.20230505.2",
+    version="10.3.20230608",
     description="Package providing common functionality for UI automation test packs",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -19,7 +19,7 @@ setup(
         "pyhamcrest==2.0.4",
         "python-dateutil==2.8.2",
         "requests==2.31.0",
-        "selenium==4.9.1",
+        "selenium==4.10.0",
         "six==1.16.0",
         "urllib3==1.26.12",
         "axe-selenium-python-nhsuk"
