3329d959d79c3beef8860c5e22db6e4ca8da8dd2
Ghufran Khan <guffyk@googlemail.com>
2024-01-10 17:39:54 +0000

    - Added the ability to specify which _rules_ to use when running Axe reports on a page. This is not a breaking change as the default is still running the report on the full page using default rules - Full Axe accessibility report is now returned regardless of whether violations are found

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="uitestcore",
-    version="10.5.20240110",
+    version="10.5.0",
     description="Package providing common functionality for UI automation test packs",
     long_description=readme,
     long_description_content_type="text/markdown",
